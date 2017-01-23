66f31e90c3aa36e4613293a7240cd8e9ec07e9f0
edc12 <edc12@in.fhkn.de>
2017-01-23 12:11:45 +0100

    erste cli implementierung

--- usb_driver/MSP430connect.py
+++ usb_driver/MSP430connect.py
@@ -1,7 +1,7 @@
 import os.path
 from MSP430 import *
 
-def cli(showMsg=""):
+def cli(msp, showMsg=""):
    cmd = input(showMsg)
    
    retval = __handle_input(cmd)
@@ -16,10 +16,10 @@ def __handle_input(cmdString):
    if(cmdString == 'hd'):
       HELP_STR = "Programm erzeugen:\n\n"
       HELP_STR += "\tes muss der Pfad zu einer .txt datei führen,\n\twelche dem TI_TXT hex format entspricht\n\n"
-      HELP_STR += "um diese zu erzeugen in den Projekteinstellungen folgendes tun:\n"
+      HELP_STR += "Um diese zu erzeugen in den Projekteinstellungen folgendes tun:\n"
       HELP_STR += "\t1. unter 'Build' Configuration auf Release stellen\n"
-      HELP_STR += "\t2. unter 'MSP430 Hex Utility' haken bei Enable MSP430 \n\tHex Utility aktivieren\n"
-      HELP_STR += "\t3. unter 'Output Format Options' das Output format auf \n\tOutput TI_TXT hex format setzen\n"
+      HELP_STR += "\t2. unter 'MSP430 Hex Utility' haken bei Enable MSP430 \n\t   Hex Utility aktivieren\n"
+      HELP_STR += "\t3. unter 'Output Format Options' das Output format auf \n\t   Output TI_TXT hex format setzen\n"
       HELP_STR += "\t4. Build Release ausführen, 'projektname'.txt liegt jetzt im workspace\n"
       print(HELP_STR)
       return 1
@@ -36,23 +36,44 @@ def __handle_input(cmdString):
       return -1
       
    if cmdString[0] == "d":
-      return __download(cmdString)
+      code, msp = __download(cmdString)
+      MSP = msp
+      return code
 
 def __download(str):
    if(len(str) < 7):
       print("Pfadangabe nötig (d C:/Users/.../out.txt)")
-      return 0
+      return 0, None
       
    if str[-4:] != ".txt":
       print("ein .txt file wird benötigt tippe hd für hilfe")
-      return 0
+      return 0, None
    
    str = str.replace("\\","/")
    
    f = None
    if not os.path.isfile(str[2:]):
       print("{} konnte nicht gefunden werden".format(str[2:]))
-      return -2
+      return -2, None
+
+   try:
+      msp.program(str[2:])
+   except:
+      return -3, None
+   print("Download erfolgreich!")
+   return 1,msp
+
+def __write(data):
+   pass
+   
+#def write(self, data):
+#      return self.ftdev.write(data)
+#   
+#   def read(self, n_bytes):
+#      return self.ftdev.read(n_bytes)
+
+def main():
+   WELCOME_STR = "MSP430 connector stellt eine Verbindung zu ihrem MSP430 her,\nmit h gelangen sie zur Hilfe\n\n"
    
    msp = None
    try:     
@@ -61,13 +82,7 @@ def __download(str):
       print(err)
       return -1
    
-   msp.program(str[2:])
-   return 1
-
-def main():
-   WELCOME_STR = "MSP430 connector stellt eine Verbindung zu ihrem MSP430 her,\nmit h gelangen sie zur Hilfe\n\n"
-     
-   retval = cli(WELCOME_STR)
+   retval = cli(msp,WELCOME_STR)
    while retval != -1:
-      retval = cli()
+      retval = cli(msp)
 main()
